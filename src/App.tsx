2982a1ddfc424699d28b4759c78dd15c629f155e
manikdevbhagat <manikdevbhagat@gmail.com>
2023-12-07 19:49:26 +0530

    Added input tab and map components

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import InputTab from "./components/Map/InputTab";
+import Map from "./components/Map/Map";
+import { Coordinate } from "./types";
 
 function App() {
+  const [paths, setPaths] = useState<Coordinate[][]>([]);
+  const [isSimulating, setIsSimulating] = useState<boolean>(false);
+  const [isPaused, setIsPaused] = useState<boolean>(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <InputTab
+        paths={paths}
+        setPaths={setPaths}
+        isSimulating={isSimulating}
+        setIsSimulating={setIsSimulating}
+        isPaused={isPaused}
+        setIsPaused={setIsPaused}
+      />
+      <Map paths={paths} isSimulating={isSimulating} isPaused={isPaused} />
     </div>
   );
 }
